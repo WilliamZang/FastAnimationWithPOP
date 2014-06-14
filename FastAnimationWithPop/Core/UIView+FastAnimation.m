8ccbf200c5687e3e060b373e30eace0dd2820577
zangcw <chengwei.zang.1985@gmail.com>
2014-06-14 14:00:57 +0800

    Fix bug https://github.com/WilliamZang/FastAnimationWithPOP/issues/6

--- FastAnimationWithPop/Core/UIView+FastAnimation.m
+++ FastAnimationWithPop/Core/UIView+FastAnimation.m
@@ -10,25 +10,25 @@
 #import <objc/runtime.h>
 
 #define DEFINE_RW_FLAG(ctype, flag, setter, asstype)            \
-    static void *flag##Key = &flag##Key;                        \
+static void *flag##Key = &flag##Key;                        \
 - (void)setter:(ctype *)value {                                 \
-    objc_setAssociatedObject(self, flag##Key, value, asstype);  \
+objc_setAssociatedObject(self, flag##Key, value, asstype);  \
 }                                                               \
 - (ctype *)flag {                                               \
-    return objc_getAssociatedObject(self, flag##Key);           \
+return objc_getAssociatedObject(self, flag##Key);           \
 }
 
 #define DEFINE_RW_CGFLOAT_FLAG(flag, setter)                    \
-    static void *flag##Key = &flag##Key;                        \
+static void *flag##Key = &flag##Key;                        \
 - (void)setter: (CGFloat)value {                                \
-    objc_setAssociatedObject(self, flag##Key,                   \
-        [NSNumber numberWithFloat:value],                       \
-        OBJC_ASSOCIATION_RETAIN_NONATOMIC);                     \
+objc_setAssociatedObject(self, flag##Key,                   \
+[NSNumber numberWithFloat:value],                       \
+OBJC_ASSOCIATION_RETAIN_NONATOMIC);                     \
 }                                                               \
 - (CGFloat)flag                                                 \
 {                                                               \
-    return [objc_getAssociatedObject(self, flag##Key)           \
-        floatValue];                                            \
+return [objc_getAssociatedObject(self, flag##Key)           \
+floatValue];                                            \
 }
 
 #define DEFINE_RW_CGFLOAT_FLAG_WITH_DEFAULT(flag, setter, default)  \
@@ -66,9 +66,28 @@ OBJC_ASSOCIATION_RETAIN_NONATOMIC);                                 \
 }                                                                   \
 - (double)flag                                                      \
 {                                                                   \
-    id value = objc_getAssociatedObject(self, flag##Key);           \
-    return value ? [value doubleValue] : default;                   \
+id value = objc_getAssociatedObject(self, flag##Key);           \
+return value ? [value doubleValue] : default;                   \
 }
+
+@implementation NSObject(FastAnimation)
+
+- (void)swizzle_awakeFromNib
+{
+    [self swizzle_awakeFromNib];
+    UIView *view = (UIView *)self;
+    if ([view isKindOfClass:UIView.class] && view.animationType) {
+        Class animationClass = NSClassFromString(view.animationType);
+        if (animationClass == nil) {
+            animationClass = NSClassFromString([@"FAAnimation" stringByAppendingString:view.animationType]);
+        }
+        NSAssert([animationClass conformsToProtocol:@protocol(FastAnimationProtocol)], @"The property 'animationType' must a class name and conforms protocol 'FastAnmationProtocol'");
+        [animationClass performAnimation:view];
+    }
+    
+}
+
+@end
 
 static void *animationParamsKey = &animationParamsKey;
 @implementation UIView (FastAnimation)
@@ -86,24 +105,13 @@ DEFINE_RW_DOUBLE_FLAG(delay, setDelay)
     return dictionary;
 }
 
-- (void)swizzle_awakeFromNib
-{
-    [self swizzle_awakeFromNib];
-    if (self.animationType) {
-        Class animationClass = NSClassFromString(self.animationType);
-        if (animationClass == nil) {
-            animationClass = NSClassFromString([@"FAAnimation" stringByAppendingString:self.animationType]);
-        }
-        NSAssert([animationClass conformsToProtocol:@protocol(FastAnimationProtocol)], @"The property 'animationType' must a class name and conforms protocol 'FastAnmationProtocol'");
-        [animationClass performAnimation:self];
-    }
-}
+
 
 + (void)load
 {
     Method original, swizzle;
     
-    original = class_getInstanceMethod(self, @selector(awakeFromNib));
+    original = class_getInstanceMethod([NSObject class], @selector(awakeFromNib));
     swizzle = class_getInstanceMethod(self, @selector(swizzle_awakeFromNib));
     method_exchangeImplementations(original, swizzle);
 }
